0ba1e3441cdb1dc7afd012906639d0cd461df364
Nicole Tan <geniusnic009@gmail.com>
2023-05-31 11:08:24 +0800

    Initial Commit

---
+++ github testing/github_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_testingApp.swift
+//  github testing
+//
+//  Created by T Krobot on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct github_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
